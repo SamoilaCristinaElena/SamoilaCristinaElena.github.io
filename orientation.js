c7d50b81cfba39208cd576f1c91762d8af90b0f3
SamoilaCristinaElena <44614220+SamoilaCristinaElena@users.noreply.github.com>
2018-10-30 10:25:18 -0700

    fix

--- orientation.js
+++ orientation.js
@@ -1,12 +1,12 @@
-document.getelementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.0";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.0";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
 function la_schimbare_gyro (e)
 {
-document .getelementById("id_x").innerHTMLL= e.beta;
-document .getelementById("id_y").innerHTMLL= e.gamma;
-document .getelementById("id_z").innerHTMLL= e.alpha;
+document .getElementById("id_x").innerHTMLL= e.beta;
+document .getElementById("id_y").innerHTMLL= e.gamma;
+document .getElementById("id_z").innerHTMLL= e.alpha;
 
 }
 }
