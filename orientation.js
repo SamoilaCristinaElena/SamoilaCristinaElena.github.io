49eac584137d9f0f447e8a19a7773f7b473e4ef0
SamoilaCristinaElena <44614220+SamoilaCristinaElena@users.noreply.github.com>
2018-10-30 10:19:15 -0700

    add new version

--- orientation.js
+++ orientation.js
@@ -1,4 +1,6 @@
-addEventListener("deviceorentation", la_schimbare_gyro);
+dcument.getelementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.0";
+
+window.addEventListener("deviceorentation", la_schimbare_gyro);
 
 function la_schimbare_gyro (e)
 {
