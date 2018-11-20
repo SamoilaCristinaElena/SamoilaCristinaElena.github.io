f84266b96853dca99399d44bdc37d56a9b9220a4
SamoilaCristinaElena <44614220+SamoilaCristinaElena@users.noreply.github.com>
2018-11-20 08:47:45 -0800

    fix

--- desenare.js
+++ desenare.js
@@ -1,7 +1,12 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.20.11.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.20.11.6";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchmouve", on_touch_mouve);
+
+
+
+
 var rect = canvas.getBoundingClientRect();
 
 
@@ -25,3 +30,19 @@ function on_touch(e)
 
 
 }
+
+function  on_touch_mouve(e)
+
+ for ( var i= 0 ; i<e.changedTouches.length; i++)
+        {
+          var context = canvas.getContext ("2d");
+              context.beginPath();
+               context.arc(  e.changedTouches.item(i).pageX - rect.left,
+                             e.changedTouches.item(i).pageY - rect.top,
+                             20,
+                             0,2 *Math.PI
+                             );
+               context.stroke ();
+     
+
+    }
