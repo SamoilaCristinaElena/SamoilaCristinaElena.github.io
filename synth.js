4de225e2da6936bd1b354dd36cfc8823aa9db257
SamoilaCristinaElena <44614220+SamoilaCristinaElena@users.noreply.github.com>
2018-12-04 08:46:18 -0800

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.10";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.11";
 var synth = window.speechSynthesis;
 
 function afiseaza_voci()
@@ -19,3 +19,11 @@ function vorbeste()
 	synth.speak(enunt);
 }
  
+//....................................
+
+
+function sfarsit_vorbeste ()
+{
+document.getElementById("id_button_vorbeste").disable = false;
+	
+}
